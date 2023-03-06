af7ef5b5a5f9f9e4cf44c2e3ba9144e424824f79
Lucas Brennand <100360881+lucas2020109662@users.noreply.github.com>
2023-03-06 09:18:49 -0300

    Finish Regressao

---
+++ Metodos-N-mericos-main/Metodos-N-mericos-main/Metodos-N-mericos-main/Metodos Numericos/src/Regressao_Linear/Main.java
@@ -0,0 +1,11 @@
+package Regressao_Linear;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] meses = {18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}; //meses da criança
+        double[] altura = {76.1, 77, 78.1, 78.2, 78.8, 79.7, 79.9, 81.1, 81.2, 81.8, 82.8, 83.5}; //altura em cm
+
+        RegressaoLinear regressaoLinear = new RegressaoLinear(meses, altura);
+        System.out.println("Yi = " + regressaoLinear.Yi(2));
+    }
+}
